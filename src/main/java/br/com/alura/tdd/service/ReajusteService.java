573cd2c57f86ca1b610ed15b8a6d3e21054bdc91
Marcos <marcosviniciohd@hotmail.com>
2023-02-09 17:41:03 -0300

    Refatorando o código utilizando TDD

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,16 +8,9 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
 	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-		if (desempenho == Desempenho.A_DESEJAR) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-			funcionario.reajustarSalario(reajuste);
-		}else if (desempenho == Desempenho.BOM) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-			funcionario.reajustarSalario(reajuste);
-		}else {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-			funcionario.reajustarSalario(reajuste);
-		}
+			BigDecimal percentual = desempenho.percentualReajuste();
+			BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+			funcionario.reajustarSalario(reajuste);	
 		
 	}
 
